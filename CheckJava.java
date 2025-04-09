ab860eb35e0e9c9ff3d969348cde3b2517d0d2ac
Gon Solo <gonsolo@gmail.com>
2025-04-09 12:36:55 +0200

    Java 24 seems to be ok.

--- CheckJava.java
+++ CheckJava.java
@@ -1,9 +1,12 @@
 class CheckJava {
         public static void main(String[] args) {
                 String version = System.getProperty("java.specification.version");
+		System.out.println(version);
                 if (version.equals("1.8")) {
                         System.out.println("Java version 8, ok!");
-                } else {
+                } else if (version.equals("24")) {
+                        System.out.println("Java version 24, ok!");
+		} else {
                         System.out.println("Java has to be version 8!");
                         System.exit(1);
                 }
